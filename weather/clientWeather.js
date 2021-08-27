c728d64f9b1d26fcf524a62906c3a30647a5ff39
tastydev <39250826+tastydev@users.noreply.github.com>
2021-08-27 15:32:12 +0200

    rem paused clock, add setMsPerGameMinute
    make game minute as long as real minute

--- weather/clientWeather.js
+++ weather/clientWeather.js
@@ -1,7 +1,5 @@
 import * as alt from 'alt';
-import * as game from 'natives';
 
-alt.onServer('disableClock', ()=>{
-    game.pauseClock(true);
-    alt.log('paused clock');
+alt.on('connectionComplete', () => {
+    alt.setMsPerGameMinute(60000);
 });
